392bbdab4080c856f8b8fea855f0373265e50a7c
Carlos Michael Rodriguez <cmr624@nyu.edu>
2020-04-29 17:47:09 -0400

    cleaner set click

--- src/edu/accessibleSprite.ts
+++ src/edu/accessibleSprite.ts
@@ -9,6 +9,8 @@ export class AccessibleSpriteButton extends BaseSprite{
     
     domElement : Phaser.GameObjects.DOMElement;
     name : string;
+
+    clickFunction : Function;
     /**
      * Base Accessible Sprite
      * Contains a DOM element that has the same width / height as our button
@@ -24,17 +26,19 @@ export class AccessibleSpriteButton extends BaseSprite{
         el.setAttribute('role', 'button');
         el.onkeydown = ((e) => {
             if ((e.which === 13) || (e.which === 32)) {
-                this.click();
+                this.clickFunction();
             }
         });
         this.domElement = this.scene.add.dom(x, y, el);
-
-        this.setInteractive();
-        this.on('pointerdown', () => this.click());
+        this.setClick(() => this.scene.subtitleManager.addSubtitle(this.scene, this.name));
     }
 
-    click(){
-        this.scene.subtitleManager.addSubtitle(this.scene, this.name);
+    setClick(fn : Function){
+        this.setInteractive();
+        this.on('pointerdown', () => {
+            fn();
+        });
+        this.clickFunction = fn;
     }
 
     moveDOMElement(x, y) {
