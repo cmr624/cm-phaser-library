5097ad13b9a5edc18ff98615a9e6c3716c46932d
Carlos Michael Rodriguez <cmr624@nyu.edu>
2020-05-15 14:53:56 -0400

    adding local storage

---
+++ src/managers/localStorage.ts
@@ -0,0 +1,52 @@
+
+
+export class Storage implements WindowLocalStorage {
+    
+    map : Map<string, BaseSaveFile>;
+    saveSlots : Array<string>;
+
+    constructor(){
+        this.localStorage = window.localStorage;
+        this.map = new Map<string, BaseSaveFile>();
+    }
+
+    localStorage: globalThis.Storage;
+
+    saveFile<T extends BaseSaveFile>(file : T) : boolean{
+        if (this.map[file.key] !== undefined) {
+            console.log('we already have this thing in our db');
+            return false;
+        }
+        this.localStorage.setItem(file.key, JSON.stringify(file));
+        this.map[file.key] = file;
+        return true;
+    }
+}
+
+class DataObjectCreator {
+    currentData : object;
+    constructor(){
+        this.currentData = {};
+    }
+
+    addItem(key, val){
+        this.currentData[key] = val;
+    }
+
+    removeItem(key) {
+        delete this.currentData[key];
+    }
+
+    returnJsonStr(){
+        return JSON.stringify(this.currentData);
+    }
+
+}
+
+// a save file has a program ID
+// and an *iterable storage structure ?*
+
+export interface BaseSaveFile {
+    programID: number;
+    key : string;
+}
